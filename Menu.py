5d4489a1c1486aa89637235da7acce9b13a43e5a
Connect2waqas <waqasnadan972@gmail.com>
2025-12-19 20:44:26 +0500

    Refactor dashboard functionality to use control module and implement booking and checking processes

--- Menu.py
+++ Menu.py
@@ -1,4 +1,4 @@
-import Descion
+import control
 def show_dashboard(username):
     print(f"\nWelcome, {username}! You are logged in.")
 
@@ -23,42 +23,11 @@ def show_dashboard(username):
             break
 
         elif choice == "1":
-             print(Descion.booking())
-            # print("Room booking module opened...")
-            # print(Greet_To_room())
-            # print(customer_management.save_user_data()) 
-            # total_bill = 0
-            # current  = payments.final_bill()
-            # current += total_bill
-            # if current >= 5000:
-            #     print(Room_confirmation())
-            #     print(f"You have Pay {current}")
-            #     print("Email recived! ")
-            #     delete_data = input("Did you want to delete your data: ").lower()
-            #     if delete_data == "yes":
-            #         customer_management.delete_customer_data()
-            #     else:
-            #         print("Thank You")
+             print(control.booking())
 
-            # else:
-            #     print("Something went wrong!")
-            # return current
-        
         elif choice == "2":
-            print("Check-in module opened...")
-            print(customer_management.processing())
-            userData = customer_management.save_user_data()
-            delete_data = input("Did you want to delete your data: ").lower()
-            if delete_data == "yes":
-                    customer_management.delete_customer_data()
-            else:
-                print("Thank You")
-            print("Profile Update Successfully...")
-
-            break
-
-
-
+            return control.checking()
+        
         # elif choice == 3:
 
         # add more later…\\\
--- control.py
+++ control.py
@@ -22,3 +22,15 @@ def booking():
                 print("Something went wrong!")
         return current
         
+def checking():
+        print("Check-in module opened...")
+        print(customer_management.processing())
+        userData = customer_management.save_user_data()
+        delete_data = input("Did you want to delete your data: ").lower()
+        if delete_data == "yes":
+            customer_management.delete_customer_data()
+        else:
+            print("Thank You")
+            print("Profile Update Successfully...")
+
+        return 1
